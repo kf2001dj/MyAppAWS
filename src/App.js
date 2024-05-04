360a428217c8c5219a3b2178c5d502db5809792b
ThanhDong <cntt23022019@gmail.com>
2024-05-04 15:10:48 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="tcom" />
+        <p>
+         Thành Đông Tcom
+        </p>
+        <a
+          className="App-link"
+          href="https://tcom.vn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Tcom
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
